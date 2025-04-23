c11d5807ae93db9ac3f774aa8b77e80ee8cf9977
Python2.0 <111122155+Fall1996@users.noreply.github.com>
2025-04-23 15:10:24 +0000

    Update ticket_generator.py

--- ticket_generator.py
+++ ticket_generator.py
@@ -7,10 +7,8 @@ class TicketCaisse:
         #self.pdf = FPDF(orientation='P', unit='mm', format=(72, 200))
         self.pdf = FPDF(format='A5')
         self.pdf.add_page()
-        # Ajout de support pour les caractères spéciaux
-        self.pdf.add_font('Arial', '', 'C:\\Windows\\Fonts\\arial.ttf', uni=True)
-        self.pdf.set_font('Arial', '', 8)
-        # self.pdf.set_font('helvetica', '', 8)
+        # Utiliser une police intégrée (Helvetica) au lieu d'Arial
+        self.pdf.set_font('Helvetica', '', 8)
         self.pdf.set_auto_page_break(auto=True, margin=10)
         self.current_y = 10
         self.produits = []
